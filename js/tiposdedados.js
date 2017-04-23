600050cbff28b2cf7ee1999911bfd9b52c2f7889
Felipe Marques <lipemarques8@gmail.com>
2017-04-23 12:41:03 -0300

    Up

---
+++ js/tiposdedados.js
@@ -0,0 +1,35 @@
+// Tipos de Dados
+
+// String
+texto = "Curso Js desde \"zer\""
+
+// Numerico
+  //Inteir
+  age = 27;
+  //Float
+  price = 14.90;
+
+// Array
+array = ["Karine","Sophia","Felipe",16,true,false];
+document.write(array[2] + " ama " + array[0]);
+// Objetos
+object = {
+  color: "Black",
+  size: 14
+}
+// Boolean
+boolean = true;
+boolean = false;
+// Undefined
+
+// Null
+
+// NaN
+
+var texto;
+var age;
+var name;
+var price;
+var array;
+var object;
+var boolean;
---
+++ js/variaveis.js
@@ -0,0 +1,15 @@
+//Comentário de Introdução ao JS
+//Concatenação Usa-se +
+
+//Variável name receberá um valor
+name = prompt("What's your name?");
+age = prompt("What's your age?");
+
+//Exibir o valor da variavel name e idade
+document.write("<h1>" + name + " " + age + "</h1>");
+console.log(name + " " + age);
+alert(name + " " + age);
+
+//Variável name sendo declarada
+var name;
+var age;
