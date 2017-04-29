02265f83f4e7f315e253db5fd443828cf7819248
Felipe Marques <lipemarques8@gmail.com>
2017-04-29 15:23:17 -0300

    Ciclo For

--- js/ciclofor.js
+++ js/ciclofor.js
@@ -1,5 +1,13 @@
 day = ["Segunda","Terça", "Quarta", "Quinta", "Sexta"];
 for(i = 0;i <= day.length -1; i++){
-  document.write(day[i] + ", ");
+  document.write(day[i] + ",<br> ");
 }
+
+alunoson = 10;
+
+for(i = 0; i <= alunoson; i++){
+  document.write(i + "<br>");
+}
+
 var day;
+var alunos;
